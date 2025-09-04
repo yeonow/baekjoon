bd73df79193f380ed0809d7fba7261b9e5ef566d
yeonow <florau@knu.ac.kr>
2025-09-04 19:22:29 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+
+	printf("%.lf\n", pow(n, 2));
+	printf("2");
+
+	return 0;
+}
