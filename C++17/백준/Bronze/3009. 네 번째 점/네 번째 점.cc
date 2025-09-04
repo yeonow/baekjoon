bd3874a6b0c4eb0a0f93dacbeb7b7ec11f3e4882
yeonow <florau@knu.ac.kr>
2025-09-04 19:32:45 +0900

    [Bronze III] Title: 네 번째 점, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/3009. 네 번째 점/네 번째 점.cc
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int x1, x2, x3, y1, y2, y3;
+	int x4, y4;
+	scanf("%d%d", &x1, &y1);
+	scanf("%d%d", &x2, &y2);
+	scanf("%d%d", &x3, &y3);
+
+	if (x1 == x2)
+	{
+		x4 = x3;
+		if (y1 == y2)
+			y4 = y3;
+		else if (y1 == y3)
+			y4 = y2;
+		else
+			y4 = y1;
+	}
+	else if (x1 == x3)
+	{
+		x4 = x2;
+		if (y1 == y2)
+			y4 = y3;
+		else if (y1 == y3)
+			y4 = y2;
+		else
+			y4 = y1;
+	}
+	else
+	{
+		x4 = x1;
+		if (y1 == y2)
+			y4 = y3;
+		else if (y1 == y3)
+			y4 = y2;
+		else
+			y4 = y1;
+	}
+	printf("%d %d", x4, y4);
+
+	return 0;
+}
